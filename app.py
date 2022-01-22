0dd9ebf3d93b3849b0e4ff54c83c236f65c95648
fallendeath <98119614+fallendeath@users.noreply.github.com>
2022-01-22 12:08:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_cors import CORS
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
+CORS(app)
